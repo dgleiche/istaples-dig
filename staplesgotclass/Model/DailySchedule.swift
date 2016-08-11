13d5339b0f6a3a073a773ce2b54ce8acebaa8fdb
dgleiche <dgleiche@me.com>
2016-08-11 01:40:42 -0400

    Started to work on DailySchedule

---
+++ staplesgotclass/Model/DailySchedule.swift
@@ -0,0 +1,22 @@
+//
+//  DailySchedule.swift
+//  staplesgotclass
+//
+//  Created by Dylan on 8/11/16.
+//  Copyright © 2016 Dylan Diamond. All rights reserved.
+//
+
+import Foundation
+
+class DailySchedule: NSObject {
+    
+    let date: NSDate
+    let isModified: Bool?
+    
+    init(date: NSDate) {
+        self.date = date
+        
+        
+    }
+    
+}
